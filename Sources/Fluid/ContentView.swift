c7d2b4d000f4c8286b2c6b38ce60b5c3eb4038ab
Shreeraman A K <shreeraman.ak@gmail.com>
2026-08-30 15:02:26 -0700

    feat(meeting): strengthen detection and unify recording overlay

--- Sources/Fluid/ContentView.swift
+++ Sources/Fluid/ContentView.swift
@@ -985,6 +985,8 @@ struct ContentView: View {
             self.navigateToApp(.aiEnhancements)
         case .history:
             self.navigateToApp(.history)
+        case .meetingTranscription:
+            self.navigateToApp(.meetingTools)
         }
     }
 
@@ -3940,7 +3942,6 @@ struct ContentView: View {
             },
             rewriteModeCallback: {
                 guard !self.presentExclusiveActivityBlockIfNeeded() else { return }
-                guard !self.showPrivateAIEditModeUnavailableIfNeeded() else { return }
 
                 self.captureRecordingContext()
 
